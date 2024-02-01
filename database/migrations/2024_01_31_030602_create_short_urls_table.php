8ed596371aa9c8cd8826ad12b99068c67530bd6a
brunovt074 <brunodev.pdl@gmail.com>
2024-02-01 00:03:50 -0300

    added uuid strategy to short_urls

--- database/migrations/2024_01_31_030602_create_short_urls_table.php
+++ database/migrations/2024_01_31_030602_create_short_urls_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('short_urls', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary;
             $table->foreignId('user_id')->nullable()->constrained();    
             $table->text('original_url');
             $table->string('short_url')->nullable();
