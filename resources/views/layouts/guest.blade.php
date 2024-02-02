3970482f2e7853c59888db565c9a1f5d5cb7725f
brunovt074 <brunodev.pdl@gmail.com>
2024-02-02 01:51:41 -0300

    welcome.blade.php justified. Color Remaining

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -9,14 +9,12 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>       
-        
-        {{-- <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
+        <title>{{ config('app.name', 'Laravel') }}</title>         
+      
     </head>
     <body>
         <header>
-            <nav class="navbar">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
                 <div class="container-fluid">
                   <a class="navbar-brand" href="#">Navbar</a>
                 </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,42 +1,24 @@
 <x-guest-layout>
-    <div class="">
-        <section class="">
-            <h1 class="text-center">Short your Link</h1>
-            {{-- @if(session('success_message'))
-                {!! session('success_message') !!}
-            @endif
-            <form method="POST" action="{{ route('short.url') }}">
+    <div class="mt-2 pt-4 pb-4">
+        <div class="container input-div pt-4 pb-4 rounded">
+            <h1 class="title text-center pt-4 pb-4">Short your Link</h1>
+            <form class="card container col-10 col-lg-7 pt-4 pb-4 rounded" method="POST" action="{{ route('short.url') }}">
                 @csrf
-                <input class="rounded-lg" type="url" name="original_url"/>
-                @error('original_url')
-                    <span class=""> {{ $message }}</span>
-                @enderror
-
-                <button class="btn btn-primary" type="submit">Short URL</button>                            
-            </form> --}}
-        </section>
-    </div>
-    {{-- <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 flex justify-center bg-white border-b border-gray-200">
-                    <section>
-                        <h1 class="text-4xl text-blue-800">Short your Link</h1>
-                        @if(session('success_message'))
+                <div class="card-body container d-inline-block justify-content-center rounded">
+                    @if(session('success_message'))
+                        <div class="alert alert-success pb-3">
                             {!! session('success_message') !!}
+                        </div>                            
                         @endif
-                        <form method="POST" action="{{ route('short.url') }}">
-                            @csrf
-                            <input class="border border-gray-300 rounded-lg" type="url" name="original_url"/>
-                            @error('original_url')
-                                <span class="text-red-400 m-2 p-2"> {{ $message }}</span>
-                            @enderror
-
-                            <button class="m-2 px-6 py-2 bg-green-500 hover:bg-green-700 rounded-lg" type="submit">Short URL</button>                            
-                        </form>
-                    </section>
+                    <input class="col-12 rounded mt-2" type="url" name="original_url"/>
+                    <button class="btn btn-primary mt-3 mb-2" type="submit">Short URL</button> 
                 </div>
-            </div>
-        </div>
-    </div>     --}}
+                <div class="d-flex justify-content-center">
+                    @error('original_url')
+                    <div class="col-6 alert alert-danger"> {{ $message }}</span>
+                    @enderror
+                </div>
+            </form>
+        </div>        
+    </div>    
 </x-guest-layout>
